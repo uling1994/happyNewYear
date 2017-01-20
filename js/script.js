2ea35c4fa3bedfc7bceac03c6aaa37d76cbfc08f
uling1994 <wanghao19940923@hotmail.com>
2017-01-20 20:10:41 +0800

    happy new year

---
+++ js/script.js
@@ -0,0 +1,26 @@
+window.onload = function () {
+    var page1 = document.getElementById("page1");
+    var page2 = document.getElementById("page2");
+    var page3 = document.getElementById("page3");
+
+    var music = document.getElementById("music");
+    var audio = document.getElementsByTagName("audio")[0];
+    music.addEventListener("touchstart", function (event) {
+        if (audio.paused) {
+            audio.play();
+        } else {
+            audio.pause();
+        };
+    }, false);
+
+    page1.addEventListener("touchstart", function () {
+        page1.style.display = "none";
+        page2.style.display = "block";
+        page3.style.display = "block";
+        page3.style.top = "100%"
+        setTimeout(function () {
+            page2.setAttribute("class", "page fadeOut");
+            page3.setAttribute("class", "page fadeIn");
+        }, 5500);
+    }, false);
+};
